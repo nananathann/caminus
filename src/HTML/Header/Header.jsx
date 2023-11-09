1acce8951811ce809a476bf4161df7ad3dba1cae
nathan <nathandiasbr17@gmail.com>
2023-11-08 21:56:52 -0300

    teste de funcionalidade dos links

--- src/HTML/Header/Header.jsx
+++ src/HTML/Header/Header.jsx
@@ -38,12 +38,12 @@ function Header() {
                 <div className={`menu ${abrirFechar == true ? "" : "close"}`}>
                     <nav>
                         <ul>
-                            <NavLink><Link to={'/'}>Tela Inicial </Link>/</NavLink>
-                            <NavLink><Link to={'/ideia'}>Ideia</Link></NavLink>
-                            <NavLink><Link to={'/objetivo'}>Objetivo</Link></NavLink>
-                            <NavLink><Link to={'/explicacao'}>Explicação</Link></NavLink>
-                            <NavLink><Link to={'/equipamentos'}>Equipamentos</Link></NavLink>
-                            <NavLink><Link to={'/contato'}>Contato</Link></NavLink>
+                            <Link to={'/'}>Tela Inicial </Link>
+                            <Link to={'/ideia'}>Ideia</Link>
+                            <Link to={'/objetivo'}>Objetivo</Link>
+                            <Link to={'/explicacao'}>Explicação</Link>
+                            <Link to={'/equipamentos'}>Equipamentos</Link>
+                            <Link to={'/contato'}>Contato</Link>
                         </ul>
 
                         <a href="https://doc-0k-30-prod-03-apps-viewer.googleusercontent.com/viewer2/prod-03/pdf/bbcunj2h3o3odbcl8mq10gcfod80dgdk/9u5fg0233av935m1ea7bsd9icsjquf7t/1692281850000/3/107439570428730947018/APznzaZO92KL9EQdCxye3yIklaf-Kd9e-E-TfQE9ztNlySuddTpUQ0ItgyBHM9tCsp2Ix_8rORVhCbjogI28pLAOU4YRupjLNrIMHgoPpi7KiF3danBeNlUigdmW27xdVaGDXcMV9A00irUAc6rJJTqbP0l0d39yjsvrQ4PybP-tE2I5YL90JP2NFTYqtZ1FORjD0y7y4yuP7pRgB45QyJe_K_MEBE81ftP9PQtSrfYEGSUH9cfgqDYYr1MaMVO69i1EOywdozSY0PyH12YuKM7I9ntL7M-S_ltmWhAVm3ADFHVxzJXqyBXjUqpioc1o7Yfw2f6AyzqIzsc6VIU7S3zK0kQ_-BAugTnWRrc-noQGHR6dnfWsQTDP7kAGogFcvpCHyeQJ24UASZ7aVapexkelLbSiTaSN-g==?authuser=1&nonce=erjdhpdi60h0k&user=107439570428730947018&hash=37ggi7tbh4qb2hdigeb8omq6dkl1lk9h">PDF DO PROJETO</a>
